17d43a9ca35866fed927dc54ba78e566a7626ee7
Liubingfeng-ice <56472470+Liubingfeng-ice@users.noreply.github.com>
2020-03-09 20:34:36 +0800

    Add files via upload

---
+++ UserController.java
@@ -0,0 +1,16 @@
+package com.liubingfeng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+
+public class UserController {
+    @RequestMapping("/get")
+    public String getUser(){
+        return "user";
+    }
+
+}
